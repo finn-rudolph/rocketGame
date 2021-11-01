365e34d68f4ac1126a5f426cec601a14212b6acf
Finn Rudolph <finn.rudolph@t-online.de>
2021-11-01 17:27:33 +0100

    Implement moving background

--- public/scripts/game/meteroid/meteroid.js
+++
@@ -1,6 +0,0 @@
-import { Actor } from "../abstractions";
-class Meteroid extends Actor {
-    constructor(positionX, positionY, velocityX, velocityY) {
-        super(positionX, positionY, velocityX, velocityY);
-    }
-}
--- public/scripts/game/rocket/rocket.js
+++
@@ -1,7 +0,0 @@
-import { Actor } from "../abstractions";
-class Rocket extends Actor {
-    constructor(positionX, positionY, velocityX, velocityY, life) {
-        super(positionX, positionY, velocityX, velocityY);
-        this.life = life;
-    }
-}
